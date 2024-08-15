a869366f7998a078587e1dace488d451cca138b7
mouryaraj214 <rajmourya00092@gmail.com>
2024-08-15 18:22:47 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,18 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Fun from './script.jsx'
+
+
+const root = createRoot(document.getElementById('root'))
+root.render(
+  <>
+  <Fun/>
+  <App/>
+  </>
+    
+    
+    
+  
+)
---
+++ src/script.jsx
@@ -0,0 +1,8 @@
+function Fun()
+ {
+    return(
+  <h2>Hi we are enjoying the react playlist</h2>
+    )
+ }
+
+export default Fun
